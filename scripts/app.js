5056a498bf00d90ec603de0fb75a71fc5648cb98
Andrew <andrew.vinocur@gmail.com>
2016-08-26 14:52:27 -0700

    place holder car images append to lane, normalize CSS now included

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+var car1;
+var car2;
+
+
+$(document).ready( function() {
+  console.log("main JS is linked");
+  $("#lane1").append('<img class=\"car1\" src="imgs/blueCar.png" />');
+  $("#lane2").append('<img class=\"car2\" src="imgs/orangeCar.png" />');
+  $("#startMsg").append("To begin, press enter");
+})
